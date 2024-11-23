1c7fa0d1cfe79d6d83cf3d35fee24137ba1c68b5
NiczSpeed <mateusz.libera01@gmail.com>
2024-11-23 21:00:30 +0100

    create hotel and room css and boostrap + cleanup

--- src/components/CreateHotelView.vue
+++ src/components/CreateHotelView.vue
@@ -5,26 +5,40 @@
         <div class="form-floating mb-3 col-md-6">
           <input type="text" class="form-control w-100" id="floatingEmail" placeholder="Name" required
             v-model="form.name" />
-          <label class="ps-4" for="floatingEmail">name</label>
+          <label class="ps-4" for="floatingEmail">Name</label>
+        </div>
+        <div class="form-floating mb-3 col-md-6">
+          <input type="text" class="form-control w-100" id="floatingAddress" placeholder="Name" required
+            v-model="form.address" />
+          <label class="ps-4" for="floatingAddress">Address</label>
         </div>
       </div>
       <div class="row g-3">
         <div class="form-floating mb-3 col-md-6">
-          <input type="text" class="form-control w-100" id="floatingEmail" placeholder="Name" required v-model="form.address"/>
-          <label class="ps-4" for="floatingEmail">address</label>
+          <input type="text" class="form-control w-100" id="floatingCity" placeholder="City" required
+            v-model="form.city" />
+          <label class="ps-4" for="floatingCity">city</label>
+        </div>
+        <div class="form-floating mb-3 col-md-6">
+          <input type="text" class="form-control w-100" id="floatingState" placeholder="State" required
+            v-model="form.state" />
+          <label class="ps-4" for="floatingState">State</label>
         </div>
       </div>
-      <label>city</label>
-      <input type="text" v-model="form.city" id="city" name="city" required />
-      <label> State </label>
-      <input type="text" v-model="form.state" id="state" name="state" required />
-      <label> number of Stars </label>
-      <input type="number" v-model="form.numberOfStars" id="numberOfStars" name="numberOfStars" required min="1"
-        max="5" />
-      <label> contact number </label>
-      <input type="tel" v-model="form.contact" id="contact" name="contact" placeholder="123456789" pattern="[0-9]{9}"
-        required />
-      <button type="submit">Create New Hotel</button>
+      <div class="row g-3">
+        <div class="form-floating mb-3 col-md-6">
+          <input type="number" class="form-control w-100" id="floatingState" placeholder="State" required
+            v-model="form.numberOfStars" min="1" max="5" />
+          <label lass="ps-4" for="floatingNumberOfStars"> Number of stars </label>
+        </div>
+        <div class="form-floating mb-3 col-md-6">
+          <input type="tel" class="form-control w-100" id="floatingState" placeholder="123456789" required
+            v-model="form.contact" pattern="[0-9]{9}" />
+          <label lass="ps-4" for="floatingContact"> Contact number </label>
+
+        </div>
+      </div>
+      <button type="submit" class="btn btn-secondary w-100">Create Hotel</button>
     </form>
   </div>
 </template>
--- src/components/CreateRoomView.vue
+++ src/components/CreateRoomView.vue
@@ -1,78 +1,68 @@
 <template>
-  <h2>Create Room</h2>
-  <form @submit.prevent="submitForm">
-    <label>number</label>
-    <input
-      type="number"
-      v-model="form.number"
-      id="number"
-      name="number"
-      required
-    />
-    <label>number of beds</label>
-    <input
-      type="number"
-      v-model="form.numberOfBeds"
-      id="numberOfBeds"
-      name="numberOfBeds"
-      required
-      min="1"
-      max="3"
-    />
-    <label> status </label>
-    <select v-model="form.status" id="status" name="status" required>
-      <option value="OK" selected>OK</option>
-      <option value="TEMPORARILY_UNAVAILABLE">TEMPORARILY_UNAVAILABLE</option>
-    </select>
-    <label> Week Price </label>
-    <input
-      type="number"
-      step="0.01"
-      v-model="form.weekPrice"
-      id="weekPrice"
-      name="weekPrice"
-      required
-    />
-    <label> Weekend Price </label>
-    <input
-      type="number"
-      step="0.01"
-      v-model="form.weekendPrice"
-      id="weekendPrice"
-      name="weekendPrice"
-      required
-    />
-    <label> Description</label>
-    <input
-      type="text"
-      v-model="form.description"
-      id="description"
-      name="description"
-      required
-    />
-    <select v-model="selectedCity" @change="fetchHotels" required>
-      <option disabled value="">Choose City</option>
-      <option v-for="(option, index) in cities" :key="index" :value="option">
-        {{ option }}
-      </option>
-    </select>
+  <div class="d-flex justify-content-center ">
+    <form class="form" @submit.prevent="submitForm">
+      <div class="row g-3">
+        <div class="form-floating mb-3 col-md-6">
+          <input type="number" class="form-control w-100" id="floatingNumber" placeholder="Number" required
+            v-model="form.number" />
+          <label class="ps-4" for="floatingNumber">Number</label>
+        </div>
+        <div class="form-floating mb-3 col-md-6">
+          <input type="number" class="form-control w-100" id="floatingNumberOfBeds" placeholder="Number of beds"
+            v-model="form.numberOfBeds" required min="1" max="3" />
+          <label class="ps-4" for="floatingNumberOfBeds">Number of beds</label>
+        </div>
+      </div>
+      <div class="row g-3">
+        <div class="form-floating mb-3 col-md-6">
+          <select class="form-control w-100"  v-model="form.status" id="status" name="status" required>
+            <option disabled value="">Status</option>
+            <option value="OK" selected>OK</option>
+            <option value="TEMPORARILY_UNAVAILABLE">TEMPORARILY_UNAVAILABLE</option>
+          </select>
+        </div>
+        <div class="form-floating mb-3 col-md-6">
+          <input type="text" class="form-control w-100" id="floatingNumber" placeholder="Number" required v-model="form.description"/>
+          <label class="ps-4" for="floatingDescription"> Description</label>
+        </div>
+      </div>
+      <div class="row g-3">
+        <div class="form-floating mb-3 col-md-6">
+          <input type="number" step="0.01" class="form-control w-100" id="floatingWeekPrice" placeholder="Number" required v-model="form.weekPrice" min="0.01"/>
+          <label class="ps-4" for="floatingWeekPrice"> Week Price </label>
+        </div>
+        <div class="form-floating mb-3 col-md-6">
+          <input type="number" step="0.01" class="form-control w-100" id="floatingWeekendPrice" placeholder="Number" required v-model="form.weekendPrice" min="0.01"/>
+          <label class="ps-4" for="floatingWeekendPrice"> Weekend Price </label>
+        </div>
+      </div>
+      <div class="row g-3">
+        <div class="form-floating mb-3 col-md-6">
+          <select class="form-control w-100" v-model="selectedCity" @change="fetchHotels" required>
+            <option disabled value="">Choose City</option>
+            <option v-for="(option, index) in cities" :key="index" :value="option">
+              {{ option }}
+            </option>
+          </select>
+        </div>
+        <div class="form-floating mb-3 col-md-6">
+          <select class="form-control w-100" v-model="form.hotel" id="hotel" name="hotel" required>
+            <option disabled value="">Choose Hotel</option>
+            <option v-for="(option, index) in hotels" :key="index" :value="option">
+              {{ option }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-secondary w-100">Create room</button>
+    </form>
+  </div>
 
-    <label>Choose hotel</label>
-    <select v-model="form.hotel" id="hotel" name="hotel" required>
-      <option disabled value="">Choose Hotel</option>
-       <option v-for="(option, index) in hotels" :key="index" :value="option">
-        {{ option }}
-      </option>
-    </select>
-
-    <button type="submit">Create New Hotel</button>
-  </form>
-  <router-link to="/">Home</router-link>
 </template>
 
 <script>
 import api from "../utils/axiosInterceptor";
-import {fetchCities} from '../utils/fetchHotelsMethods'
+import { fetchCities } from '../utils/fetchHotelsMethods'
 
 export default {
   name: "RegisterView",
@@ -84,7 +74,7 @@ export default {
       form: {
         number: "",
         numberOfBeds: "",
-        status:"",
+        status: "",
         weekPrice: "",
         weekendPrice: "",
         description: "",
@@ -93,13 +83,13 @@ export default {
     };
   },
   async created() {
-    try{
+    try {
       this.cities = await fetchCities();
-    }catch (error){
+    } catch (error) {
       console.error("There was an error with fetch list of cities! Error: ", error);
       throw error;
     }
-   
+
   },
   methods: {
     async submitForm() {
@@ -144,5 +134,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
